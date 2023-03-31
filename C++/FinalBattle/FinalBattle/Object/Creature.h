64981c37f072f940b2ebe900fe8917c6b69aff4a
skytoy12 <129160465+skytoy12@users.noreply.github.com>
2023-03-31 16:24:29 +0900

    &
    &

--- C++/FinalBattle/FinalBattle/Object/Creature.h
+++ C++/FinalBattle/FinalBattle/Object/Creature.h
@@ -7,9 +7,9 @@ public :
 	virtual void Damaged(float amount, shared_ptr<Creature> attacker = nullptr);
 	virtual void Attack(shared_ptr<Creature> victim)abstract;
 	bool IsAlive();
-	const float GetHp() { return _hp; }
-	const float GetAtk() { return _atk; }
-	const int GetAgr() { return _aggravation; }
+	const float& GetHp() { return _hp; }
+	const float& GetAtk() { return _atk; }
+	const int& GetAgr() { return _aggravation; }
 	const string& Getname() { return _name; }
 	const CreatureType& GetType() { return _type; }
 	int RandomNum(int min, int max);

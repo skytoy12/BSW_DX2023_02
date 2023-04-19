e7ca4abde588dbf84106d93bb666811789ad51d9
skytoy12 <129160465+skytoy12@users.noreply.github.com>
2023-04-19 13:49:20 +0900

    priority_queue 구현
    priority_queue 구현

--- WIN_API/WIN_API_2023_02/Algorithm/PriorityQueue.cpp
+++ WIN_API/WIN_API_2023_02/Algorithm/PriorityQueue.cpp
@@ -10,26 +10,73 @@
 
 using namespace std;
 
+template <typename T>
 class Priority_Queue
 {
 public :
 	// 부모가 인덱스 n일 때
 	// 자식은 n * 2 + 1, n * 2 + 2다
-	void push(const int& value) 
+	void push(const T& value)
 	{
 		container.push_back(value);
+		for (int i = 0; i < container.size(); i++)
+		{
+			if ((i * 2) + 1 >= container.size())
+				return;
+			if ((i * 2) + 2 >= container.size())
+			{
+				if (container[i] < container[(i * 2) + 1])
+				{
+					swap(container[i], container[(i * 2) + 1]);
+				}
+				return;
+			}
+			if (container[i] < container[(i * 2) + 1] || container[i] < container[(i * 2) + 2])
+			{
+				if (container[(i * 2) + 1] >= container[(i * 2) + 2])
+					swap(container[i], container[(i * 2) + 1]);
+				else
+					swap(container[i], container[(i * 2) + 2]);
+			}
+		}
 	}
 
-	void pop() {}
+	void pop()
+	{
+		if (container.empty() == true)
+			return;
+		swap(container[0], container.back());
+		container.resize(container.size() - 1);
+		for (int i = 0; i < container.size(); i++)
+		{
+			if ((i * 2) + 1 >= container.size())
+				return;
+			if ((i * 2) + 2 >= container.size())
+			{
+				if (container[i] < container[(i * 2) + 1])
+				{
+					swap(container[i], container[(i * 2) + 1]);
+				}
+				return;
+			}
+			if (container[i] < container[(i * 2) + 1] || container[i] < container[(i * 2) + 2])
+			{
+				if (container[(i * 2) + 1] >= container[(i * 2) + 2])
+					swap(container[i], container[(i * 2) + 1]);
+				else
+					swap(container[i], container[(i * 2) + 2]);
+			}
+		}
+	}
 
-	const int& top() { return 0; }
+	const T& top() { return container[0]; }
 
-	unsigned int size() { return 0; }
+	unsigned int size() { return container.size(); }
 
-	bool empty() { return false; }
+	bool empty() { return container.size() == 0; }
 
 private :
-	vector<int> container;
+	vector<T> container;
 };
 
 int main()
@@ -40,7 +87,7 @@ int main()
 	// 힙이론
 	// 
 
-	priority_queue<int> pq;
+	Priority_Queue<int> pq;
 	pq.push(10);
 	pq.push(7);
 	pq.push(20);

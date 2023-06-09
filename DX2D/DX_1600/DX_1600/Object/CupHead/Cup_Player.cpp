a9590107599d2ee40b658607fee7359edd5b00e0
BSW <129160465+skytoy12@users.noreply.github.com>
2023-06-09 13:40:54 +0900

    cuphead run 구현

--- DX2D/DX_1600/DX_1600/Object/CupHead/Cup_Player.cpp
+++ DX2D/DX_1600/DX_1600/Object/CupHead/Cup_Player.cpp
@@ -7,7 +7,8 @@ Cup_Player::Cup_Player()
 {
 	_collider = make_shared<CircleCollider>(50);
 
-	CreateAction();
+	CreateIdleAction();
+	CreateRunAction();
 
 	_transform = make_shared<Transform>();
 	_transform->SetParent(_collider->GetTransform());
@@ -21,18 +22,36 @@ Cup_Player::~Cup_Player()
 
 void Cup_Player::Update()
 {
+	Select();
+
 	_collider->Update();
 
-	_action->Update();
-	_sprite->Update();
+	_actions[_state]->Update();
+
+	if (_state == Cup_Player::State::IDLE)
+		_idleSprite->Update();
+
+	if (_state == Cup_Player::State::RUN)
+		_runSprite->Update();
+
 	_transform->Update();
 }
 
 void Cup_Player::Render()
 {
 	_transform->SetBuffer(0);
-	_sprite->SetCurFrame(_action->GetCurClip());
-	_sprite->Render();
+
+	if (_state == Cup_Player::State::IDLE)
+	{
+		_idleSprite->SetCurFrame(_actions[_state]->GetCurClip());
+		_idleSprite->Render();
+	}
+
+	if (_state == Cup_Player::State::RUN)
+	{
+		_runSprite->SetCurFrame(_actions[_state]->GetCurClip());
+		_runSprite->Render();
+	}
 
 	_collider->Render();
 }
@@ -43,7 +62,36 @@ void Cup_Player::PostRender()
 	//_transform->SetPosition(_fixedPos);
 }
 
-void Cup_Player::CreateAction()
+void Cup_Player::Select()
+{
+
+	if (KEY_PRESS('A'))
+	{
+		_state = Cup_Player::State::RUN;
+		Vector2 movePos = Vector2(-500.0f, 0.0f) * DELTA_TIME;
+		Move(movePos);
+	}
+
+	if (KEY_PRESS('D'))
+	{
+		_state = Cup_Player::State::RUN;
+		Vector2 movePos = Vector2(500.0f, 0.0f) * DELTA_TIME;
+		Move(movePos);
+	}
+
+	if (KEY_UP('D'))
+	{
+		_state = Cup_Player::State::IDLE;
+	}
+
+	if (KEY_UP('A'))
+	{
+		_state = Cup_Player::State::IDLE;
+
+	}
+}
+
+void Cup_Player::CreateIdleAction()
 {
 	wstring srvPath = L"Resource/CupHead/Idle.png";
 	shared_ptr<SRV> srv = ADD_SRV(srvPath);
@@ -74,7 +122,44 @@ void Cup_Player::CreateAction()
 		row = row->NextSiblingElement();
 	}
 
-	_action = make_shared<Action>(clips, "Cup_IDLE");
-	_action->Play();
-	_sprite = make_shared<Sprite>(srvPath, Vector2(250, 250));
+	shared_ptr<Action> action = make_shared<Action>(clips, "Cup_IDLE");
+	action->Play();
+	_actions.push_back(action);
+	_idleSprite = make_shared<Sprite>(srvPath, Vector2(250, 250));
+}
+
+void Cup_Player::CreateRunAction()
+{
+	wstring srvPath = L"Resource/CupHead/Run.png";
+	shared_ptr<SRV> srv = ADD_SRV(srvPath);
+
+	shared_ptr<tinyxml2::XMLDocument> document = make_shared<tinyxml2::XMLDocument>();
+	string path = "Resource/CupHead/Run.xml";
+	document->LoadFile(path.c_str());
+
+	XMLElement* texturAtlas = document->FirstChildElement();
+	XMLElement* row = texturAtlas->FirstChildElement();
+
+	vector<Action::Clip> clips;
+
+	while (true)
+	{
+		if (row == nullptr)
+			break;
+
+		int x = row->FindAttribute("x")->IntValue();
+		int y = row->FindAttribute("y")->IntValue();
+		int w = row->FindAttribute("w")->IntValue();
+		int h = row->FindAttribute("h")->IntValue();
+
+		Action::Clip clip = Action::Clip(x, y, w, h, srv);
+		clips.push_back(clip);
+
+		row = row->NextSiblingElement();
+	}
+
+	shared_ptr<Action> action = make_shared<Action>(clips, "CUP_RUN");
+	action->Play();
+	_runSprite = make_shared<Sprite>(srvPath, Vector2(120, 120));
+	_actions.push_back(action);
 }
--- DX2D/DX_1600/DX_1600/Object/CupHead/Cup_Player.h
+++ DX2D/DX_1600/DX_1600/Object/CupHead/Cup_Player.h
@@ -2,6 +2,14 @@
 class Cup_Player
 {
 public :
+
+	enum State
+	{
+		IDLE,
+		RUN,
+		NONE
+	};
+
 	Cup_Player();
 	~Cup_Player();
 
@@ -9,16 +17,25 @@ public :
 	void Render();
 	void PostRender();
 
-	void CreateAction();
+	void Move(Vector2 movePos) { _collider->GetTransform()->AddVector2(movePos); }
+	void Select();
+
+	void CreateIdleAction();
+	void CreateRunAction();
 
 	void SetPosition(Vector2 pos) { _collider->SetPosition(pos); }
 
 private :
+	State _state = Cup_Player::State::IDLE;
+
 	shared_ptr<CircleCollider>_collider;
 
-	shared_ptr<Action> _action;
+	vector<shared_ptr<Action>> _actions;
 
-	shared_ptr<Sprite> _sprite;
+
+	shared_ptr<Sprite> _idleSprite;
+	shared_ptr<Sprite> _runSprite;
+
 	shared_ptr<Transform> _transform;
 
 	Vector2 _fixedPos;

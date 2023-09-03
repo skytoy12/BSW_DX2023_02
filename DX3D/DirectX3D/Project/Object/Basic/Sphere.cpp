0d0feba27745f48c7e22ed79d49674c0e9a67005
skytoy12 <129160465+skytoy12@users.noreply.github.com>
2023-09-04 01:59:09 +0900

    sphere
    sphere

--- DX3D/DirectX3D/Project/Object/Basic/Sphere.cpp
+++ DX3D/DirectX3D/Project/Object/Basic/Sphere.cpp
@@ -7,7 +7,10 @@ Sphere::Sphere(Vector4 color, float radius)
 	{
 		Circle* circle = new Circle(color, radius);
 		circles.push_back(circle);
+		if (i > 0)
+			circles[i]->SetParent(circles[0]);
 	}
+
 
 	for (int i = 0; i < 720; i++)
 	{
@@ -21,6 +24,7 @@ Sphere::Sphere(Vector4 color, float radius)
 
 	for (int i = 1440; i < 2160; i++)
 	{
+		circles[i]->rotation.y = Ridan(90);
 		circles[i]->rotation.z = Ridan(0.5f * i);
 	}
 }
@@ -33,6 +37,27 @@ void Sphere::Update()
 {
 	for (Circle* circle : circles)
 		circle->Update();
+
+	if (KEY_PRESS('Q'))
+	{
+		circles[0]->rotation.y -= Time::Delta();
+	}
+
+	if (KEY_PRESS('E'))
+	{
+		circles[0]->rotation.y += Time::Delta();
+	}
+
+	if (KEY_PRESS('W'))
+	{
+		circles[0]->rotation.x += Time::Delta();
+	}
+
+	if (KEY_PRESS('S'))
+	{
+		circles[0]->rotation.x -= Time::Delta();
+	}
+
 }
 
 void Sphere::Render()
